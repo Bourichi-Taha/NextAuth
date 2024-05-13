74376967f42cad1d15591bd5744ebdc9a175a91a
bourichi taha <bourichi.taha@gmail.com>
2024-05-13 16:16:43 +0100

    admin  page

---
+++ components/auth/role-gate.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import useCurrentRole from "@/hooks/use-current-role";
+import { UserRole } from "@prisma/client";
+import FormError from "../form-error";
+
+interface RoleGateProps {
+    children: React.ReactNode;
+    allowedRole: UserRole;
+}
+
+const RoleGate = (props:RoleGateProps) => {
+
+    const {allowedRole,children} = props;
+
+    const role = useCurrentRole();
+
+    if (role !== allowedRole) {
+        return (
+            <FormError
+                message="You do not have permission to view this content!"
+            />
+        )
+    }
+
+  return (
+    <>
+        {
+            children
+        }
+    </>
+  )
+}
+
+export default RoleGate
